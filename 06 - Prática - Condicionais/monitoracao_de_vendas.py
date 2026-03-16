fe08d3b1d759a3ee77ba2f2784a1829819927238
clmateus <cl.mateuscosta@gmail.com>
2026-03-16 17:54:16 -0300

    exercicio 1

---
+++ 06 - Prática - Condicionais/monitoracao_de_vendas.py
@@ -0,0 +1,13 @@
+# Exercícios
+try:
+    macas = int(input("Digite a quantidade de maçãs vendidas: "))
+    bananas = int(input("Digite a quantidade de bananas vendidas: "))
+
+    if macas > bananas:
+        print("As maçãs tiveram mais vendas.")
+    elif macas < bananas:
+        print("As bananas tiveram mais vendas.")
+    else:
+        print("As vendas de maçãs e bananas empataram.")
+except ValueError:
+    print("Erro! É necessário inserir um número inteiro.")
