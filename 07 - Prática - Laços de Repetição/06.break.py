154b797f3b0b20f17e2baaef8d383dead1c3c7d4
clmateus <cl.mateuscosta@gmail.com>
2026-03-17 13:46:51 -0300

    break

---
+++ 07 - Prática - Laços de Repetição/06.break.py
@@ -0,0 +1,8 @@
+livros = ["1984", "Dom Casmurro", "O Pequeno Príncipe", "O Hobbit", "Orgulho e Preconceito"]
+
+livro_desejado = "O Hobbit"
+
+for i in livros:
+    if i == livro_desejado:
+        print(f"Livro encontrado: {i}")
+        break
