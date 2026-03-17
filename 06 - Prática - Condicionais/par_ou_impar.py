3c0d28f50581758a5fdb5332e512bbbec62856ef
clmateus <cl.mateuscosta@gmail.com>
2026-03-17 09:03:23 -0300

    par ou impar

---
+++ 06 - Prática - Condicionais/par_ou_impar.py
@@ -0,0 +1,10 @@
+try:
+    numero = int(input("Digite um número inteiro: "))
+
+    if (numero % 2) == 0:
+        print("O número é par.")
+    else:
+        print("O número é impar.")
+        
+except:
+    print("Erro! Digite um número inteiro.")
