51651a93148223a76daa666e97014cc709275c02
clmateus <cl.mateuscosta@gmail.com>
2026-03-18 17:25:10 -0300

    fixed directory

---
+++ 01 - Manipulação de Strings/app.py
@@ -0,0 +1,10 @@
+print("Restaurant Manager\n")
+
+print("1. Cadastrar restaurante")
+print("2. Listar restaurantes")
+print("3. Ativar restaurante")
+print("4. Sair\n")
+
+opcao_escolhida = input("Digite a opção desejada: ")
+
+print(f"Você escolheu a opção {opcao_escolhida}.") 
