e709114895a6255900cd7a465823d990d1643c6e
clmateus <cl.mateuscosta@gmail.com>
2026-03-18 11:38:26 -0300

    classe musica

---
+++ 10 - Programação Orientada a Objetos/modelos/musica.py
@@ -0,0 +1,19 @@
+class Musica():
+    nome = ''
+    artista = ''
+    duracao = int
+
+south_of_heaven = Musica()
+south_of_heaven.nome = "South of Heaven"
+south_of_heaven.artista = "Slayer"
+south_of_heaven.duracao = 298
+
+crystal_mountain = Musica()
+crystal_mountain.nome = "Crystal Mountain"
+crystal_mountain.artista = "Death"
+crystal_mountain.duracao = 307
+
+nutshell = Musica()
+nutshell.nome = "Nutshell"
+nutshell.artista = "Alice in Chains"
+nutshell.duracao = 259
