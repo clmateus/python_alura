9d539e79f65f7ede10727d6efb1dc1daec16311d
clmateus <cl.mateuscosta@gmail.com>
2026-03-18 15:41:22 -0300

    classmethod, metodo do objeto e toggle

--- 10 - Programação Orientada a Objetos/modelos/restaurante.py
+++ 10 - Programação Orientada a Objetos/modelos/restaurante.py
@@ -3,18 +3,20 @@ class Restaurante():
 
     # __init__ - Método que o Python executa automaticamente no exato momento da criação da instância de um objeto.
     def __init__(self, nome, categoria):
-        self.nome = nome
-        self.categoria = categoria
-        self._ativo = True
+        self.nome = nome.title()
+        self.categoria = categoria.upper()
+        self._ativo = False
         Restaurante.restaurantes.append(self)
 
     # __str__ - Método que define o que o Python irá retornar quando o objeto for chamado.
     def __str__(self):
         return f'{self.nome} | {self.categoria} | {self._ativo}'
     
-    def listar_restaurantes():
+    # @classmethod - Decorator que transforma um método genérico em um método da classe, em vez de um método do objeto.
+    @classmethod
+    def listar_restaurantes(cls):
         print(f'{"Nome do Restaurante".ljust(25)} | {"Categoria".ljust(25)} | Ativo')
-        for restaurante in Restaurante.restaurantes:
+        for restaurante in cls.restaurantes:
             print(f'{restaurante.nome.ljust(25)} | {restaurante.categoria.ljust(25)} | {restaurante.ativo}')
 
     # @property - Decorator que transforma um método em um atributo inteligente.
@@ -22,23 +24,27 @@ class Restaurante():
     def ativo(self):
         return '☑' if self._ativo else '☐'
 
-restaurante_1 = Restaurante('McDonalds', 'Fast Food')
-# restaurante_1.nome = "McDonalds"
-# restaurante_1.categoria = "Fast Food"
-# restaurante_1.ativo = True
+    def alternar_estado(self):
+        self._ativo = not self._ativo
 
-restaurante_2 = Restaurante('Burger King', 'Fast Food')
-# restaurante_2.nome = "Burger King"
-# restaurante_2.categoria = "Fast Food"
-# restaurante_2.ativo = False
+restaurante1 = Restaurante('McDonalds', 'Fast Food')
+restaurante1.alternar_estado()
+# restaurante1.nome = "McDonalds"
+# restaurante1.categoria = "Fast Food"
+# restaurante1.ativo = True
+
+restaurante2 = Restaurante('Burger King', 'Fast Food')
+# restaurante2.nome = "Burger King"
+# restaurante2.categoria = "Fast Food"
+# restaurante2.ativo = False
 
 # dir - Lista todos os atributos de um objeto. Ideal para usar quando não conhecemos a classe.
-# print(dir(restaurante_1))
+# print(dir(restaurante1))
 
 # # vars - Cria um dicionário com os atributos de um objeto.
-# print(vars(restaurante_1))
+# print(vars(restaurante1))
 
-# print(restaurante_1)
-# print(restaurante_2)
+# print(restaurante1)
+# print(restaurante2)
 
 Restaurante.listar_restaurantes()
