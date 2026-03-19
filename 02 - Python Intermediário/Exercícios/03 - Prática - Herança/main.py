0f164ae04e15f5936ad3ffed56e8f8832d42fc92
clmateus <cl.mateuscosta@gmail.com>
2026-03-19 15:33:28 -0300

    exercícios de herança

---
+++ 02 - Python Intermediário/Exercícios/03 - Prática - Herança/main.py
@@ -0,0 +1,18 @@
+from carro import Carro
+from moto import Moto
+
+carro1 = Carro('Honda', 'Fit', 4)
+carro2 = Carro('Fiat', 'Argo', 4)
+carro3 = Carro('Hyundai', 'Veloster', 3)
+
+moto1 = Moto('Honda', 'CG', 'Casual')
+moto2 = Moto('Honda', 'Biz', 'Casual')
+moto3 = Moto('BMW', 'R1300', 'Esportiva')
+
+print(carro1)
+print(carro2)
+print(carro3)
+
+print(moto1)
+print(moto2)
+print(moto3)
