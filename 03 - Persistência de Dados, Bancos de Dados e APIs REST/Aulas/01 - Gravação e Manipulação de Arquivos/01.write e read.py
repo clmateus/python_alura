f08c6a6c039265b80facb90558697c5fe03832ea
clmateus <cl.mateuscosta@gmail.com>
2026-03-20 15:30:55 -0300

    update

--- 03 - Persistência de Dados, Bancos de Dados e APIs REST/Aulas/01 - Gravação e Manipulação de Arquivos/01.write e read.py
+++ 03 - Persistência de Dados, Bancos de Dados e APIs REST/Aulas/01 - Gravação e Manipulação de Arquivos/01.write e read.py
@@ -1,6 +1,6 @@
 # mode='w' - Write - Modo de escrita de arquivos. Se o arquivo não existir ele é criado, se já existe é sobreescrito.
-# with open('arquivo.txt', mode='w', encoding='utf8') as fp:
-#     fp.write('Olá mundo!')
+with open('arquivo.txt', mode='w', encoding='utf8') as fp:
+    fp.write('Olá mundo!')
 
 # mode='r' - Read - Modo de leitura de arquivos. 
 with open('arquivo.txt', mode='r', encoding='utf8') as fp:
