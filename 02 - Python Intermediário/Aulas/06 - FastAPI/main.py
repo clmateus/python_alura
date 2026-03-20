259e4f86564b0c4e8ce4be550b0fa3fee10fb17c
clmateus <cl.mateuscosta@gmail.com>
2026-03-20 13:56:31 -0300

    added comments

--- 02 - Python Intermediário/Aulas/06 - FastAPI/main.py
+++ 02 - Python Intermediário/Aulas/06 - FastAPI/main.py
@@ -1,7 +1,12 @@
+# fastapi - Biblioteca que serve para criar APIs
 from fastapi import FastAPI
 
+# Cria o aplicativo principal da API e atribui a variável app
 app = FastAPI()
 
+# Diz que a próxima função vai responder no endereço '/api/hello'
 @app.get('/api/hello')
+# Função que será executada quando acessarem o endereço acima
 def hello_world():
+    # Retorna a mensagem abaixo em formato de dicionário (JSON)
     return {'Hello':'World'}
