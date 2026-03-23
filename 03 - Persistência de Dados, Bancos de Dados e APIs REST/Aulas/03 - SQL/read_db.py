7ce81213530f611f7f57a985c1b2b25b3681d33f
clmateus <cl.mateuscosta@gmail.com>
2026-03-23 14:35:52 -0300

    read and write db

---
+++ 03 - Persistência de Dados, Bancos de Dados e APIs REST/Aulas/03 - SQL/read_db.py
@@ -0,0 +1,18 @@
+import sqlite3
+
+conn = sqlite3.connect("escola.db")
+cursor = conn.cursor()
+
+cursor.execute(
+    """
+        SELECT * FROM estudantes
+    """
+)
+
+conn.commit()
+estudantes = cursor.fetchall()
+
+for estudante in estudantes:
+    print(estudante)
+
+conn.close()
